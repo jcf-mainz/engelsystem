3a762c6129719cd6159d487c9ec78a5ac97dc0a7
Nihlatak <nilathakgame@googlemail.com>
2016-11-16 16:00:57 +0100

    Addition of Notification variable

--- config/config.default.php
+++ config/config.default.php
@@ -41,6 +41,7 @@ $enable_description_jobs = true;
 $enable_age = true;
 $enable_phone = true;
 $enable_angeltypedescription = true;
+$enable_unnecessary_Notifications = false;
 
 // Number of shifts to freeload until helper is locked for shift signup.
 $max_freeloadable_shifts = 2;
