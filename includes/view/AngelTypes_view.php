117071589039307bb4f5638ee13d81cb048da7be
Nihlatak <nilathakgame@googlemail.com>
2016-11-16 16:15:48 +0100

    dect remove
    manually removed

--- includes/view/AngelTypes_view.php
+++ includes/view/AngelTypes_view.php
@@ -146,13 +146,21 @@ function AngelType_view($angeltype, $members, $user_angeltype, $admin_user_angel
       $members_confirmed[] = $member;
     }
   }
-
+if ($enable_dect) {
   $table_headers = [
       'Nick' => _("Nick"),
       'DECT' => _("DECT"),
       'actions' => ''
   ];
-
+}
+else
+{
+	  $table_headers = [
+      'Nick' => _("Nick"),
+	  'DECT' => _("Sum"),
+      'actions' => ''
+	];
+}
   if ($angeltype['requires_driver_license'] && ($coordinator || $admin_angeltypes)) {
     $table_headers = [
         'Nick' => _("Nick"),
